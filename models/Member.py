55740ce28b453d72e4cc47013d312aecf3ce55ac
Zach Banducci <zbducci@gmail.com>
2019-07-27 14:00:59 -0500

    Removed python backend

--- models/Member.py
+++
@@ -1,5 +0,0 @@
-class Member:
-    def __init__(self, **kwargs):
-        self.name = kwargs['name']
-        self.id = kwargs['id']
-        self.spotify_user = kwargs['spotify_user']
--- run_prod.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-gunicorn app:app -c gunicorn.py.ini --worker-class gevent --bind 0.0.0.0:5000&
-disown
