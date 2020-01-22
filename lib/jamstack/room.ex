ae6b8d8886b30e67e4311e0d5e156a62d9f5278a
Zachary Banducci <zbducci@gmail.com>
2020-01-21 18:11:56 -0600

    Fixed some core functions in room management GenServers

--- lib/jamstack/room.ex
+++ lib/jamstack/room.ex
@@ -1,10 +1,62 @@
 defmodule Jamstack.Room do
-  defstruct(
-    :room_code,
-    :room_name,
+  use GenServer
+
+  defstruct([
+    :code,
+    :name,
     :current_song,
     super_bump_enabled: false,
     members: [],
     queue: []
-  )
+  ])
+  
+  def start_link(room_data = %{"code" => room_id}) do
+    case GenServer.start_link(Jamstack.Room.Server, room_data, name: via_tuple(room_id)) do
+      {:ok, pid} -> {:ok, pid, room_id}
+      {:error, reason} -> raise RuntimeError, message: reason
+    end
+  end
+
+  def bump_song(room_id, song_id) do
+    if exists?(room_id) do
+      GenServer.cast(via_tuple(room_id), {:bump_song, song_id})
+    else 
+      {:error, "Room does not exist"}
+    end
+  end
+
+  def add_song(room_id, song) do
+    if exists?(room_id) do
+      GenServer.cast(via_tuple(room_id), {:add_song, song})
+    else 
+      {:error, "Room does not exist"}
+    end
+  end
+
+  def add_member(room_id, member) do
+    if exists?(room_id) do
+      GenServer.cast(via_tuple(room_id), {:add_member, member})
+    else 
+      {:error, "Room does not exist"}
+    end
+  end
+
+  def retrieve_state(room_id) do
+    if exists?(room_id) do
+      GenServer.call(via_tuple(room_id), {:retrieve_state})
+    else 
+      {:error, "Room does not exist"}
+    end
+  end
+  
+  defp via_tuple(room_id) do
+    {:via, Registry, {Jamstack.Rooms.Registry, room_id}}
+  end
+
+  defp exists?(room_id) do 
+    case Registry.lookup(Jamstack.Rooms.Registry, room_id) do
+      [] -> false
+      _ -> true
+    end
+  end
 end
--- lib/jamstack/room/server.ex
+++ lib/jamstack/room/server.ex
@@ -1,37 +1,52 @@
 defmodule Jamstack.Room.Server do
-  use GenServer
-  alias Queuhub.Room
-  alias Queuhub.Song
+  alias Jamstack.Room
+  alias Jamstack.Song
   
-  def start_link(_) do
-    GenServer.start_link(__MODULE__, %Room{})
+  def init(room_data) do
+    code = room_data |> Map.get("code")
+    name = room_data |> Map.get("name")
+
+    {:ok, %Room{ code: code, name: name }}
   end
 
-  def init(room) do
-    {:ok, room}
-  end
-
-  def handle_cast({:add_member, member}, _from, state = %Room{ members: members }) do
+  def handle_cast({:add_member, member}, state = %Room{ members: members }) do
     {:noreply, %Room{ state | members: [member | members] }}
   end
   
-  def handle_cast({:queue_song, song}, _from, state = %Room{ queue: queue }) do
+  def handle_cast({:queue_song, song}, state = %Room{ queue: queue }) do
     {:noreply, %Room{ state | queue: queue ++ [song] }}
   end
   
-  def handle_cast({:bump_song, song_id}, _from, state = %Room{ queue: queue, super_bump_enabled: true }) do
-    queue
-    |> Enum.map(song -> possibly_bump_song(song, song_id) end)
-    |> Enum.sort((a, b) -> a.bumps > b.bumps)
+  def handle_cast({:bump_song, song_id}, state = %Room{ queue: queue, super_bump_enabled: true }) do
+    new_queue =
+      queue
+      |> Enum.map(fn song -> possibly_bump_song(song, song_id) end)
+      |> Enum.sort(fn (a, b) -> a.bumps > b.bumps end)
+  
+    {:noreply, %Room{ state | queue: new_queue }}
   end
 
-  def handle_cast({:bump_song, song_id}, _from, state = %Room{ queue: queue }) do
-    
+  def handle_cast({:bump_song, song_id}, state = %Room{ queue: queue }) do
+    new_queue = 
+      queue
+      |> bump_song(song_id)
+
+    {:noreply, %Room{ state | queue: new_queue }}
   end
   
+  def handle_call({:retrieve_state}, _from, state) do
+    {:reply, state, state}
+  end
+
+  ## Helper functions ##
+
   defp possibly_bump_song(song = %Song{ uri: song_id, bumps: bumps }, song_id) do
     %Song{ song | bumps: bumps + 1}
   end
 
   defp possibly_bump_song(song, _), do: song
+  
+  defp bump_song([h | []], _), do: [h]
+  defp bump_song([other, song = %Song{uri: song_id} | tail], song_id), do: [song_id, other | tail]
+  defp bump_song([head, next | tail], song_id), do: [head, next | bump_song(tail, song_id)]
 end
