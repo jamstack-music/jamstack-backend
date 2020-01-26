d6e1717060c165f73eff02361b55fe6549757cc4
Zachary Banducci <zbducci@gmail.com>
2020-01-26 15:43:09 -0600

    Removed https

--- config/dev.exs
+++ config/dev.exs
@@ -17,12 +17,6 @@ config :jamstack, Jamstack.Repo,
 # with webpack to recompile .js and .css sources.
 config :jamstack, JamstackWeb.Endpoint,
   http: [port: 4000],
-  https: [
-   port: 4001,
-   cipher_suite: :strong,
-   keyfile: "priv/cert/selfsigned_key.pem",
-   certfile: "priv/cert/selfsigned.pem"
-  ],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
