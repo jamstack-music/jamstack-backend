0c59cf91697c854856d1bf92e406e5b84e240ff7
Zachary Banducci <zbducci@gmail.com>
2020-01-26 18:08:52 -0600

    Fixed jamstack room routes

--- lib/jamstack_web/controllers/room_controller.ex
+++ lib/jamstack_web/controllers/room_controller.ex
@@ -1,9 +1,28 @@
-defmodule QueuhubWeb.RoomController do
+defmodule JamstackWeb.RoomController do
   use JamstackWeb, :controller
 
   alias Jamstack.Rooms
+  alias Jamstack.Room
 
-  def index(conn, _) do
-    conn
+  def show(conn, %{ "id" => id }) do
+    case Room.retrieve_state(id) do
+      room_state ->
+        conn
+        |> render("show.json", %{room: room_state})
+      {:error, reason} ->
+        conn
+        |> send_resp(400, reason)
+    end
+  end
+  
+  def create(conn, data) do
+    case Jamstack.Rooms.Supervisor.create_room(data) do
+      {:ok, _pid, room_code} -> 
+        conn
+        |> send_resp(201, room_code)
+      {:error, reason} -> 
+        conn
+        |> send_resp(400, "Room could not be created")
+    end
   end
 end
